65a7d547c43d693c1a6dba50656b281437c50204
dramaticLemon <chebandima27@gmail.com>
2025-06-20 13:05:17 +0300

    add logic Quin Move

--- src/main/java/com/example/figures/Queen.java
+++ src/main/java/com/example/figures/Queen.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.example.Coordinate;
+import com.example.ValidateCoordinate;
 import com.example.config.Color;
 
 public class Queen extends Figure{
@@ -21,12 +22,69 @@ public class Queen extends Figure{
 
     @Override
     public void mekeMove(Coordinate coordinate) {
-        logger.debug("Queen is move");
+        this.file = coordinate.getColumn();
+        this.runk = coordinate.getRow();
     }
     
     @Override
     public Set<Coordinate> getPossibleMooves(Figure[][] board) {
+
         Set<Coordinate> posibleMove = new HashSet<>();
+        int rows = board.length;
+        int cols = board[0].length;
+        
+        
+        
+        int[][] directionsLikeBishop = {{-1, 1}, {1, -1}, {-1, -1}, {1, 1}};
+
+        for (int[] direction : directionsLikeBishop) {
+            int dr = direction[0];
+            int dc = direction[1];
+            
+
+            int r = this.runk + dr;
+            int c = this.file + dc;
+            
+            while (r >= 0 && r < rows && c >= 0 && c < cols) {
+                Figure target = board[r][c];
+                if (target == null) {
+                    posibleMove.add(new ValidateCoordinate(r, c));
+                } else {
+                    if (target.color != this.color) {
+                        posibleMove.add(new ValidateCoordinate(r, c));
+                    }
+                    break; // в любом случае — своя или вражеская — стоп
+            }
+            r += dr;
+            c += dc;
+            }
+        }
+    
+        int[][] directionsLikeRook = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        for (int[] direction : directionsLikeRook) {
+            int dr = direction[0];
+            int dc = direction[1];
+            
+            int r = this.runk + dr;
+            int c = this.file + dc;
+            
+            while (r >= 0 && r < rows && c >= 0 && c < cols) {
+                if (board[r][c] == null) {
+                    posibleMove.add(new ValidateCoordinate(r, c));
+                }
+                else {
+                    if (board[r][c].color != this.color) {
+                        posibleMove.add(new ValidateCoordinate(r, c));
+                        break;
+
+                    }
+                }
+                r += dr;
+                c += dc;
+            }
+        }
+
         return posibleMove;
     }
 }
